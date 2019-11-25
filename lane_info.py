b6978df6b330013d435cb7177c638291dcecf2c3
Graham McKinlay <3449059+gmckinlay@users.noreply.github.com>
2019-11-25 12:07:26 +0000

    Refactored to add a lane information class

---
+++ lane_info.py
@@ -0,0 +1,6 @@
+
+class LaneInfo:
+
+    def __init__(self, gpioPin, laneId):
+        self.gpioPin = gpioPin
+        self.laneId = laneId
--- reed_timer.py
+++ reed_timer.py
@@ -1,20 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 import datetime
+from lane_info import LaneInfo
 from multiprocessing.dummy import Pool as ThreadPool
 
 laneOne = 17
 laneTwo = 18
+maxLaps = 10
 
 def timeLaps(laneInfo):
+    # type: (LaneInfo) -> None
+    """Times the laps for a given lane"""
     prevInput = 0
     lapCount = 0
     startLapTime = datetime.datetime.now()
 
-    while lapCount < laneInfo['maxLaps']:
+    while lapCount < maxLaps:
         #take a reading
-        input = GPIO.input(laneInfo['gpioPin'])
-
+        input = GPIO.input(laneInfo.gpioPin)
+        
         #if the last reading was low and this one high, print
         if ((not prevInput) and input):
             #increment lap count
@@ -23,18 +27,26 @@ def timeLaps(laneInfo):
             endLapTime = datetime.datetime.now()
             #get lap delta time
             lapDelta = endLapTime - startLapTime
-            #print lap info
-            print("Car " + laneInfo['laneId'] + " - Lap " + str(lapCount) + " " + str(lapDelta.total_seconds()))
+            processLap(laneInfo.laneId, lapCount, lapDelta)
             #reset timer for next lap
             startLapTime = datetime.datetime.now()
+            #wait a short period to avoid double counting a lap..
             time.sleep(0.05)
 
         prevInput = input
 
+def processLap(laneId, lap, lapDelta):
+    # type: (str, int, datetime) -> None
+    """Times the laps for a given lane"""
+    #print lap info
+    print("Car " + laneId + " - Lap " + str(lap) + " " + str(lapDelta.total_seconds()))
+
 def startRace(threads=2):
     pool = ThreadPool(threads)
 
-    lanes = [{'gpioPin': laneOne, 'laneId':"1", 'maxLaps':10}, {'gpioPin': laneTwo, 'laneId':"2", 'maxLaps':10}]
+    # lanes = [{'gpioPin': laneOne, 'laneId':"1"}, {'gpioPin': laneTwo, 'laneId':"2"}]
+    lanes = [LaneInfo(laneOne, "1"), LaneInfo(laneTwo, "2")]
+    
     pool.map(timeLaps, lanes)
     pool.close()
     pool.join()
